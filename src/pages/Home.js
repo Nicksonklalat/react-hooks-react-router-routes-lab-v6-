cd4851d51b1ba42b5a635d96fe5b94b63fc82573
Nickson <lalatnickson@gmail.com>
2024-08-20 20:39:56 +0300

    Basic Routes Lab

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
+
+function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((r) => r.json())
+      .then((data) => setData(data));
+  }, []);
+  console.log(data);
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Home Page</h1>
+        {data.map((items) => (
+          <MovieCard key={items.id} title={items.title} id={items.id} />
+        ))}
+      </main>
+    </>
+  );
+}
+
+export default Home;
